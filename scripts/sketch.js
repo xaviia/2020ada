0d09f91db1c6a137ea30a334bf7ffd0e48d9f93c
Xavia Chen <xraypink@gmail.com>
2020-04-14 23:56:42 +0800

    stay cool

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,10 +1,14 @@
-let fontBold, fontRegu, fontThin;
+let fontBold, fontRegu, fontThin, forImg;
+let backgroundColor, textColor
 
 function preload() {
-  fontBold = loadFont('fonts/Proxima/Proxima Nova Bold.otf');
+  fontExtra = loadFont('fonts/Montserrat/Montserrat-ExtraBold.ttf');
+  fontBold = loadFont('fonts/Montserrat/Montserrat-Bold.ttf');
   fontRegu = loadFont('fonts/Proxima/ProximaNova-Regular.otf');
   fontThin = loadFont('fonts/Proxima/Proxima Nova Alt Thin.otf');
-  tcThin = loadFont('fonts/KozGoPr6N/KozGoPr6N-Light.otf');
+  tcBold = loadFont('fonts/KozGoPr6N/KozGoPr6N-Bold.otf');
+  forImg = loadImage('images/forImg.png');
+
 }
 
 function Character(
@@ -44,11 +48,16 @@ let num1 = 65,
   num3 = 65;
 
 function setup() {
+  backgroundColor = color(83, 15, 255)
+  backgroundColor.setAlpha(0)
+  textColor = color(0, 0, 255)
+  textColor.setAlpha(0)
+  tint(255,0)
   createCanvas(windowWidth, windowHeight);
 }
 
 function drawPointer({
-  color = [255, 100, 100],
+  color = [83, 15, 255],
   width = 20,
   height = 20
 } = {}) {
@@ -58,7 +67,14 @@ function drawPointer({
 }
 
 function draw() {
-  curvertexWidepositive()
+  blendMode(BLEND);
+
+  if (windowWidth <= 1306) {
+    drawMobileTablet()
+  } else {
+    drawDesktop()
+  }
+
   drawPointer()
 
   if ((x1 < x2 && x3 < x1) || (x1 > x2 && x3 > x1)) {
@@ -78,20 +94,17 @@ function draw() {
 
 function drawCharacter({
   character,
-  color = 0,
-  font,
+  color = [83, 15, 255],
+  font = fontBold,
   size = 300,
 } = {}) {
   if (!character) return
 
   noStroke();
-  fill(color);
+  fill(...color);
   textAlign(CENTER, CENTER);
   textSize(size);
-
-  if (font) {
-    textFont(font);
-  }
+  textFont(font);
 
   text(character.character, character.xAxisPosition, character.yAxisPosition);
 
@@ -163,16 +176,145 @@ function THIRD() {
   y3 += b3;
 }
 
-function curvertexWidepositive() {
-  background(255);
-  // FIRST();
-  // SECOND();
-  // THIRD();
-  characters.forEach(character => drawCharacter({ character }))
+function drawTabletText(){
+  blendMode(DIFFERENCE);
+  noStroke();
+  fill(textColor);
+  textSize(100);
+  textAlign(LEFT, BOTTOM);
+  textFont(fontExtra);
+  text('2020', 40 + 0.002 * mouseX, windowHeight - 350 + 0.002 * mouseY);
+  text('ADA', 40 + 0.002 * mouseX, windowHeight - 250 + 0.002 * mouseY);
+  text('AWARDS', 40 + 0.002 * mouseX, windowHeight - 150 + 0.002 * mouseY);
 
+  textSize(50);
+  textFont(fontBold);
+  text('emerging architects', 40 + 0.002 * mouseX, windowHeight - 30 + 0.002 * mouseY);
+
+  textSize(50);
+  textAlign(RIGHT, TOP);
+  textFont(fontBold);
+  text('menu', windowWidth - 40 + 0.002 * mouseX, 20 + 0.002 * mouseY);
+  
+  textSize(50);
+  textAlign(LEFT, TOP);
+  textFont(fontBold);
+  text('ADA', 40 + 0.002 * mouseX, 20 + 0.002 * mouseY);
+
+  textSize(40);
+  textAlign(LEFT, TOP);
+  textFont(tcBold);
+  text('新銳建築獎', 170 + 0.002 * mouseX, 30 + 0.002 * mouseY);
+
+  image(forImg, 40 , windowHeight - 160 + 0.002 * mouseY);
+
+}
+
+function drawMobileText(){
+  blendMode(DIFFERENCE);
+  noStroke();
+  fill(textColor);
+  textSize(50);
+  textAlign(LEFT, BOTTOM);
+  textFont(fontExtra);
+  text('2020', 10 + 0.002 * mouseX, windowHeight - 180 + 0.002 * mouseY);
+  text('ADA', 10 + 0.002 * mouseX, windowHeight - 130 + 0.002 * mouseY);
+  text('AWARDS', 10 + 0.002 * mouseX, windowHeight - 80 + 0.002 * mouseY);
+
+  textSize(28);
+  textFont(fontBold);
+  text('emerging architects', 10 + 0.002 * mouseX, windowHeight - 10 + 0.002 * mouseY);
+
+  textSize(30);
+  textAlign(RIGHT, TOP);
+  textFont(fontBold);
+  text('menu', windowWidth - 10 + 0.002 * mouseX, 10 + 0.002 * mouseY);
+  
+  textSize(30);
+  textAlign(LEFT, TOP);
+  textFont(fontBold);
+  text('ADA', 10 + 0.002 * mouseX, 10 + 0.002 * mouseY);
+
+  textSize(23);
+  textAlign(LEFT, TOP);
+  textFont(tcBold);
+  text('新銳建築獎', 83 + 0.002 * mouseX, 18 + 0.002 * mouseY);
+
+  forImg.resize(88.8, 44.4)
+  image(forImg, 10 , windowHeight - 80 + 0.002 * mouseY);
+}
+
+function drawDesktopText(){
+  blendMode(DIFFERENCE);
+  noStroke();
+  fill(textColor);
+  textSize(50);
+  textAlign(LEFT, BOTTOM);
+  textFont(fontExtra);
+  text('2020 ADA AWARDS', 40 + 0.002 * mouseX, windowHeight - 30 + 0.002 * mouseY);
+
+  textSize(50);
+  textAlign(RIGHT, BOTTOM);
+  textFont(fontBold);
+  text('emerging architects', windowWidth - 40 + 0.002 * mouseX, windowHeight - 30 + 0.002 * mouseY);
+
+  textSize(50);
+  textAlign(RIGHT, TOP);
+  textFont(fontBold);
+  text('menu', windowWidth - 40 + 0.002 * mouseX, 20 + 0.002 * mouseY);
+  
+  textSize(50);
+  textAlign(LEFT, TOP);
+  textFont(fontBold);
+  text('ADA', 40 + 0.002 * mouseX, 20 + 0.002 * mouseY);
+
+  textSize(40);
+  textAlign(LEFT, TOP);
+  textFont(tcBold);
+  text('新銳建築獎', 170 + 0.002 * mouseX, 30 + 0.002 * mouseY);
+
+  image(forImg, windowWidth - 730 , windowHeight - 100 + 0.002 * mouseY);
+
+}
+
+function drawMobileTablet() {
+  background(255);
+  background(backgroundColor);
+  characters.forEach(character => drawCharacter({ character }))
+  
   noFill();
-  stroke(255, 255, 150);
-  strokeWeight(300);
+  stroke(255, 74, 39);
+  strokeWeight(400);
+  strokeJoin(ROUND);
+  strokeCap(ROUND);
+
+  beginShape();
+  curveVertex(mouseY - 100, 15);
+  curveVertex(mouseX - 100, 15);
+  //curveVertex(windowWidth / 4, mouseX - 100);
+  //curveVertex(windowWidth / 4, mouseY - 100);
+  // curveVertex(windowWidth / 2, mouseX);
+  //curveVertex(windowWidth / 2, mouseX);
+  // curveVertex(mouseX + 700, windowHeight / 2);
+  //curveVertex(mouseY + 700, windowHeight / 2);
+  curveVertex(mouseY + 200, windowHeight - 100);
+  curveVertex(mouseX + 200, windowHeight - 100);
+  endShape();
+
+  if(windowWidth < 592)
+    drawMobileText()
+  else
+    drawTabletText()
+}
+
+function drawDesktop() {
+  background(255);
+  background(backgroundColor);
+  characters.forEach(character => drawCharacter({ character }))
+  
+  noFill();
+  stroke(255, 74, 39);
+  strokeWeight(400);
   strokeJoin(ROUND);
   strokeCap(ROUND);
 
@@ -181,15 +323,37 @@ function curvertexWidepositive() {
   curveVertex(mouseX - 100, 15);
   curveVertex(windowWidth / 4, mouseX - 100);
   curveVertex(windowWidth / 4, mouseY - 100);
-  // curveVertex(windowWidth / 2, mouseX);
   curveVertex(windowWidth / 2, mouseX);
-  // curveVertex(mouseX + 700, windowHeight / 2);
+  curveVertex(windowWidth / 2, mouseX);
+  curveVertex(mouseX + 700, windowHeight / 2);
   curveVertex(mouseY + 700, windowHeight / 2);
   curveVertex(mouseY + 200, windowHeight - 100);
   curveVertex(mouseX + 200, windowHeight - 100);
   endShape();
+
+  drawDesktopText();
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
+
+function mouseWheel(event) {
+  const prevAlpha = alpha(backgroundColor)
+  const delta = event.deltaY
+  let nextAlpha = prevAlpha + (delta / windowHeight) * 255 * 0.2
+
+  if (nextAlpha > 255) {
+    nextAlpha = 255
+  } else if (nextAlpha < 0) {
+    nextAlpha = 0
+  }
+
+  backgroundColor.setAlpha(nextAlpha)
+  textColor.setAlpha(nextAlpha)
+  tint(255, nextAlpha)
+
+  return false
+}
+
+// 0~255/windowHeight
